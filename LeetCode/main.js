e7c5fde7ed431ffa7f5f58b15badcede4aff25c6
Vladislav Sakhno <sahno3659@gmail.com>
2024-04-03 11:29:21 -0700

    add tooltip

--- LeetCode/main.js
+++ LeetCode/main.js
@@ -1,29 +1,37 @@
 let first_line_data = [
     { id: 1, img: "./image/tank 1.png", text: "T-34" },
     { id: 2, img: "./image/tank 1.png", text: "T-34" },
-    { id: 3, img: "./image/tank 1.png", text: "T-34" }
-];
-let second_line_data = [
+    { id: 3, img: "./image/tank 1.png", text: "T-34" },
     { id: 4, img: "./image/tank 1.png", text: "T-34" },
     { id: 5, img: "./image/tank 1.png", text: "T-34" },
     { id: 6, img: "./image/tank 1.png", text: "T-34" }
+];
+let second_line_data = [
+
 ];
 
 function build_line(line, imgArr) {
     let line_block = document.getElementsByClassName(line);
     imgArr.forEach(element => {
         let tank = document.createElement('div');
-        tank.className = 'tank_block';
+        tank.className = 'tank_block tooltip';
+        let tooltip = document.createElement('div');
+        tooltip.className = 'tooltip_block';
+        tooltip.innerText = "TEST";
         let img = document.createElement('img');
         let text = document.createElement('span');
         text.innerText = element.text;
         img.src = element.img;
         tank.appendChild(img);
         tank.appendChild(text);
+        tank.appendChild(tooltip);
         line_block[0].appendChild(tank);
     });
 
 }
 
+
+
+
+
 build_line('first_line', first_line_data);
-build_line('second_line', second_line_data);
